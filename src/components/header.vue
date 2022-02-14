216c71611f3af123e29de26905922784cd5aa271
Adner16 <giovanni.renda16@gmail.com>
2022-02-14 17:47:16 +0100

    added script

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,11 +1,23 @@
 <template>
-  
+    <div>
+        <input v-model="this.movie" type="text">
+        <button @click="findMovie">VAI</button>
+    </div>
+    
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            movie: '',
+        }
+    }
+    // methods:{
+            
+    //     }
+    }
 
-}
 </script>
 
 <style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -1,21 +1,36 @@
 <template>
     <ul>
         <li class="card" v-for="(movie,index) in movies" :key='index'>
-            {{}}
+            {{movie.title}}
+    <span>{{movie.vote_average}}</span>
+    <p>{{movie.original_title}}</p>
+    <p>{{movie.original_language}}</p>
         </li>
     </ul>
 </template>
 
 <script>
+    import axios from 'axios';
+
 export default {
     data(){
         return{
             movies: [],
+            api: 'c15aa4b26195705c0e005c100069298c',
         };
     },
-    mounted(){
-        axios.get().then((res) => {this.movies})
-    }
+    methods: {
+        callApi (query){
+                    axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${this.api}`)
+                    .then((res) => {
+                    this.movies = res.data.results;
+                })
+            }
+    },
+    mounted() { 
+        this.callApi('ciao');
+    },
+    
 }
 </script>
 
