a5f50ea04ddf6eaea8aa97746358eb93a9d570fc
Adner16 <giovanni.renda16@gmail.com>
2022-02-14 15:18:13 +0100

    added html

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,13 +1,24 @@
 <template>
-  
+    <ul>
+        <li class="card" v-for="(movie,index) in movies" :key='index'>
+            {{}}
+        </li>
+    </ul>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            movies: [],
+        };
+    },
+    mounted(){
+        axios.get().then((res) => {this.movies})
+    }
 }
 </script>
 
-<style>
+<style scoped lang='scss'>
 
 </style>
